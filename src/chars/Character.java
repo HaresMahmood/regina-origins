ae4d79d9fe0303351a2c95b0c2af0b6ff76da583
Dean Reynolds <dean2000r@gmail.com>
2023-10-25 14:58:34 +0100

    Restructured character player hierarchy

--- src/chars/Character.java
+++ src/chars/Character.java
@@ -1,31 +1,25 @@
 package chars;
 
+import utils.BoardPosition;
+
 // Teams is fd for me, you can continue without me
 
-public abstract class Character {
-    protected Position position;
+public abstract class Character implements IBoardPiece {
+    protected BoardPosition position;
     protected String name;  
-    protected char symbol; 
+    protected char symbol;
 
     // Constructor
-    public Character(Position position) {
+    public Character(BoardPosition position) {
         this.position = position;
     }
 
     // Getters & setters
-    public Position getPosition() {
+    public BoardPosition getPosition() {
         return this.position;
     }
 
-    public void setPosition(int x, int y) {
-        position.setX(x);
-        position.setY(y);
-    }
 
-    // TODO: Explain
-    public void setPosition(Position position) {
-        this.position = position;
-    }
 
     public String getName() {
         return this.name;
--- src/chars/Player.java
+++ src/chars/Player.java
@@ -1,10 +1,21 @@
 package chars;
 
+import utils.BoardPosition;
+
 public class Player extends Character {
     // Constructor
-    public Player(Position position) {
+    public Player(BoardPosition position) {
         super(position);
         this.name = "Donald";
         this.symbol = 'P';
+    }
+
+    public void move(int x, int y) {
+        position.setX(x);
+        position.setY(y);
+    }
+
+    public void move(BoardPosition position) {
+        this.position = position;
     }
 }
