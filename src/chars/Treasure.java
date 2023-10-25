b99f801d4d4a8c95ca9818c41290c61ab8ebcb90
Hares Mahmood <hmahmood229@gmail.com>
2023-10-25 15:36:19 +0100

    Add docs for `Treasure`

--- src/chars/Treasure.java
+++ src/chars/Treasure.java
@@ -2,8 +2,17 @@ package chars;
 
 import board.BoardPosition;
 
+/**
+ * Treasure class represents a character in the game that holds a donut stash.
+ * It extends the Character class and sets the name to "Donut Stash" and the symbol to 'T'.
+ * @param position The position of the Treasure on the game board.
+ */
 public class Treasure extends Character {
 
+    /**
+     * Constructor for the Treasure class.
+     * @param position The position of the Treasure on the game board.
+     */
     public Treasure(BoardPosition position) {
         super(position);
         this.name = "Donut Stash";
