21e85e47833fd8767d72defcdd310b96264763fa
Hares Mahmood <hmahmood229@gmail.com>
2023-10-25 15:34:43 +0100

    Add docs for `IBoardPiece`

--- src/chars/IBoardPiece.java
+++ src/chars/IBoardPiece.java
@@ -2,8 +2,27 @@ package chars;
 
 import board.BoardPosition;
 
+/**
+ * The IBoardPiece interface represents a game piece on a board.
+ * It provides methods to get the position, name, and symbol of the piece.
+ */
 public interface IBoardPiece {
+    
+    /**
+     * Returns the position of the board piece.
+     * @return the position of the board piece
+     */
     BoardPosition getPosition();
+    
+    /**
+     * Returns the name of the board piece.
+     * @return the name of the board piece
+     */
     String getName();
+    
+    /**
+     * Returns the symbol of the board piece.
+     * @return the symbol of the board piece
+     */
     char getSymbol();
 }
