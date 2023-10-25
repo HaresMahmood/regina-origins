bcdec9de2aefb622c779280aa0cbabe7f39b656a
Dean Reynolds <dean2000r@gmail.com>
2023-10-25 15:24:02 +0100

    Added board to game file

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
-public class Game {
-    // needs a board at some point
+import board.Board;
 
+public class Game {
+
+    private Board board;
+
+    public Game(Board board) {
+        this.board = board;
+    }
 
     public void play(){
         Scanner scanner = new Scanner(System.in);
         while(true){
+            board.printBoard();
             System.out.println("Where would you like to move next? (Enter quit to exit): ");
             String userInput = scanner.nextLine().toLowerCase();
             
@@ -19,7 +26,7 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = new Game(new Board(5));
         game.play();
     }
 }
--- src/board/Board.java
+++ src/board/Board.java
@@ -1,10 +1,25 @@
 package board;
 
+import chars.IBoardPiece;
+
 public class Board {
     
-    private Character[][] grid;
+    private IBoardPiece[][] grid;
 
     public Board(int size) {
-        this.grid = new Character[size][size];
+        this.grid = new IBoardPiece[size][size];
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[i][j] != null) {
+                    System.out.print(grid[i][j].getSymbol());
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
